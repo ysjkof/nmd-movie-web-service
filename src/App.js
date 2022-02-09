d552c10f64960ffe6b2af8f71867626c59d72ff2
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-02-09 19:50:25 +0900

    Navigiation 비활성화

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
 import "./App.css";
 import Todo from "./routes/Todo";
+import Coin from "./routes/Coin";
 
 function App() {
   return (
     <HashRouter>
-      <Navigation />
+      {/* <Navigation /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/todo" element={<Todo />} />
+        <Route path="/coin" element={<Coin />} />
       </Routes>
     </HashRouter>
   );
